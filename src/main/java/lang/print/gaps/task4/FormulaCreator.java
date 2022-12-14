55a64f173a1a2fde5119a52cd127b9f4d291bd3f
Sanjar Totliboyev <2110110@newuzbekistanuniversity.uz>
2022-12-14 15:19:59 +0500

    just

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,8 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        double a = 5,b=3;
-        double answer = (9*Math.pow(a,2)-5*b+2+a-7)*((a+b-4*a*b)/2);
+        double a=5,b=3;
+        double answer = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
         System.out.println(answer);
     }
 }
